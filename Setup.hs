8aec8403832e7613c1cd8fb9c7c55d947a35cb14
Aditya Siram <aditya.siram@gmail.com>
2018-01-20 09:05:59 -0600

    Fix optimization flags. Add Cabal 2.0 compatibility

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Main (main) where
 
 import Distribution.Simple
@@ -15,8 +16,14 @@ fluidToHaskellProgram :: Program
 fluidToHaskellProgram =
   (simpleProgram "fltkhs-fluidtohs") { programFindVersion = (\_ _ -> return Nothing) }
 
+-- cabal >=2.0.0.2
+#if defined(MIN_VERSION_Cabal) && MIN_VERSION_Cabal(2,0,0)
+ppFluidToHaskell :: BuildInfo -> LocalBuildInfo -> ComponentLocalBuildInfo -> PreProcessor
+ppFluidToHaskell bi lbi cli =
+#else
 ppFluidToHaskell :: BuildInfo -> LocalBuildInfo -> PreProcessor
 ppFluidToHaskell bi lbi =
+#endif
   PreProcessor
     { platformIndependent = True
     , runPreProcessor = \(inBaseDir, inRelativeFile) (outBaseDir, outRelativeFile) verbosity -> do
